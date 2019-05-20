bd3f3017fda24dcafba485d5cbccf0d32326c28b
Adrian <adr.lar@me.com>
2019-05-20 17:03:11 +0200

    use API
    uses the typeform API instead of a downloaded excel file

--- inventory_survey.py
+++ inventory_survey.py
@@ -4,6 +4,8 @@
 #Author: Adrian Lärkeryd <adrian.larkeryd@scilifelab.uu.se>
 
 import os
+import requests
+from datetime import datetime
 import pandas as pd
 import xlsxwriter
 
@@ -18,7 +20,9 @@ from reportlab.pdfbase.ttfonts import TTFont
 from functools import partial
 from svglib.svglib import svg2rlg
 
-def header(canvas, doc, content):
+from api_key import INVENTORY_API_KEY
+
+def header(canvas, doc, content, footer_string):
 	canvas.saveState()
 	w, h = content.wrap(doc.width, doc.topMargin)
 	content.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - h)
@@ -30,15 +34,18 @@ def header(canvas, doc, content):
 	canvas.setStrokeColor("#999999")
 	canvas.drawPath(p, stroke=1)
 	canvas.restoreState()
-	canvas.drawString(150, 65, "Technology Needs Inventory")
+	canvas.drawString(30, 30, footer_string)
 
-def generatePdf(user_responses):
-	doc = BaseDocTemplate(u"user_survey_plots/Technology_Needs_Inventory{}.pdf".format(user_responses["01_id"].lower().replace(" ", "_")),
+def generate_pdf(user_id, response, form_data):
+	# print form_data
+	title = form_data["title"]
+	doc = BaseDocTemplate(u"user_survey_plots/{}_{}.pdf".format(title.lower().replace(" ", "_"), user_id.lower().replace(" ", "_")),
 		pagesize=A4,
 		rightMargin=18*mm, leftMargin=18*mm,
 		topMargin=16*mm, bottomMargin=16*mm, 
 		showBoundary=0
 	)
+
 	pdfmetrics.registerFont(TTFont('MinionPro', 'MinionPro-Regular.ttf'))
 	pdfmetrics.registerFont(TTFont('Frutiger-65-Bold', 'Frutiger-LT-Std-65-Bold.ttf'))
 	pdfmetrics.registerFont(TTFont('Frutiger-45-Light', 'Frutiger-LT-Std-45-Light.ttf'))
@@ -49,113 +56,114 @@ def generatePdf(user_responses):
 	styles.add(ParagraphStyle(name="onepager_text", parent=styles["Normal"], fontName="MinionPro", fontSize=10, bold=0, color="#000000", leading=14))
 	styles.add(ParagraphStyle(name="onepager_footnote", parent=styles["Normal"], fontName="MinionPro", fontSize=7, bold=0, color="#000000", leading=14))
 	
-	frame1 = Frame(doc.leftMargin, doc.bottomMargin, doc.width/2-3.5*mm, doc.height-18*mm, id='col1', leftPadding=0*mm, topPadding=0*mm, rightPadding=0*mm, bottomPadding=0*mm)
-	frame2 = Frame(doc.leftMargin+doc.width/2+3.5*mm, doc.bottomMargin, doc.width/2-3.5*mm, doc.height-18*mm, id='col2', leftPadding=0*mm, topPadding=0*mm, rightPadding=0*mm, bottomPadding=0*mm)
-	
-	header_content = Paragraph(
-		u'<b>{} {}</b><br/><font name=Frutiger-45-Light size=12> {}</font>'.format(user_responses["02_name"], user_responses["04_email"], user_responses["03_position"]), 
-		styles["onepager_title"])
-	footer_content = Paragraph(
-		u'<b>{}</b>'.format("Technology Needs Inventory"), 
-		styles["onepager_text"])
+	frame1 = Frame(doc.leftMargin, doc.bottomMargin, doc.width/2-3.5*mm, doc.height-21*mm, id='col1', leftPadding=0*mm, topPadding=0*mm, rightPadding=0*mm, bottomPadding=0*mm)
+	frame2 = Frame(doc.leftMargin+doc.width/2+3.5*mm, doc.bottomMargin, doc.width/2-3.5*mm, doc.height-21*mm, id='col2', leftPadding=0*mm, topPadding=0*mm, rightPadding=0*mm, bottomPadding=0*mm)
+	# print "\n\n\n"
+	name_question = None
+	email_question = None
+	position_question = None
 
-	header_item = PageTemplate(id='test', frames=[frame1,frame2], onPage=partial(header, content=header_content))
-	doc.addPageTemplates([header_item])
+	for question in form_data["fields"]:
+		if name_question == None and "name" in question["title"].lower():
+			name_question = question["ref"]
+		if email_question == None and "email" in question["title"].lower():
+			email_question = question["ref"]
+		if position_question == None and "position" in question["title"].lower():
+			position_question = question["ref"]
+
+	name_response = ""
+	email_response = ""
+	position_response = ""
+
+	shown_responses = {}
+
+	for q_response in response:
+		if q_response["field"]["ref"] == name_question:
+			name_response = q_response["text"]
+		elif q_response["field"]["ref"] == email_question:
+			email_response = q_response["email"]
+		elif q_response["field"]["ref"] == position_question:
+			position_response = q_response["text"]
+		else:
+			shown_responses[q_response["field"]["ref"]] = q_response
+
+
+
+	header_content = Paragraph(
+		u'<b>{}</b><br/><font name=Frutiger-45-Light size=12>{}<br/>{}</font>'.format(name_response, email_response, position_response), 
+		styles["onepager_title"])
+
+	footer_string = title
 	
+	header_item = PageTemplate(id='inventory_survey', frames=[frame1,frame2], onPage=partial(header, content=header_content, footer_string=footer_string))
+	doc.addPageTemplates([header_item])
+
 	Story = []
 
-	Story.append(Paragraph("<font color='#95C11E' name=Frutiger-65-Bold><b>Affiliation:</b></font>", styles["onepager_inner_heading"]))
-	for affiliation in user_responses["05_affiliation"].split(";"):
-		Story.append(Paragraph(u"{}".format(affiliation), 
-			styles["onepager_text"]))
-
-	Story.append(Paragraph("<font color='#95C11E' name=Frutiger-65-Bold><b>I have previously used these platforms at SciLifeLab:</b></font>", styles["onepager_inner_heading"]))
-	for platform in user_responses["06_prev_platforms"].split(";"):
-		Story.append(Paragraph(u"{}".format(platform), 
-			styles["onepager_text"]))
-
-	if user_responses["08_representing"]:
-		Story.append(Paragraph("<font color='#95C11E' name=Frutiger-65-Bold><b>I propose a technology/service:</b></font>", styles["onepager_inner_heading"]))
-		Story.append(Paragraph(u"{}".format(user_responses["07_propose"].replace("(specify  below)", "(specified below)")), 
-			styles["onepager_text"]))
-		Story.append(Paragraph(u"{}".format(user_responses["08_representing"]), 
-			styles["onepager_text"]))	
-	else:
-		Story.append(Paragraph("<font color='#95C11E' name=Frutiger-65-Bold><b>I propose a technology/service:</b></font>", styles["onepager_inner_heading"]))
-		Story.append(Paragraph(u"{}".format(user_responses["07_propose"]), 
-			styles["onepager_text"]))
-
-
-	
-	# print user_responses["09_description"].split("\n")
-	Story.append(Paragraph("<font color='#95C11E' name=Frutiger-65-Bold><b>Give a brief description of the technology/service suggested. Specify what makes it nationally unique and the added value for academic researchers, healthcare and industry.</b></font>", styles["onepager_inner_heading"]))
-	for para in user_responses["09_description"].split("\n"):
-		Story.append(Paragraph(u"{}".format(para), 
-			styles["onepager_text"]))	
-	
-	Story.append(Paragraph("<font color='#95C11E' name=Frutiger-65-Bold><b>In which of the existing SciLifeLab Platforms would the technology/service fit?</b></font>", styles["onepager_inner_heading"]))
-	for platform in user_responses["10_platform_fit"].split(";"):
-		Story.append(Paragraph(u"{}".format(platform), 
-			styles["onepager_text"]))
-	
-	Story.append(Paragraph("<font color='#95C11E' name=Frutiger-65-Bold><b>Estimate the number of annual users of the technology if incorporated into the SciLifeLab infrastructure and offering nation-wide service (1-10, 10-100, more than 100, Don't know):</b></font>", styles["onepager_inner_heading"]))
-	Story.append(Paragraph(u"{}".format(user_responses["11_num_users"]), 
-		styles["onepager_text"]))
-	
-	# print user_responses["12_additional_comments"].split("\n")
-	Story.append(Paragraph("<font color='#95C11E' name=Frutiger-65-Bold><b>Additional comments:	</b></font>", styles["onepager_inner_heading"]))
-	for para in user_responses["12_additional_comments"].split("\n"):
-		Story.append(Paragraph(u"{}".format(para), 
-			styles["onepager_text"]))	
-
-	# Story.append(HRFlowable(width="40%", thickness=0.5, lineCap='round', color="#999999", spaceBefore=1, spaceAfter=1, hAlign='LEFT', vAlign='BOTTOM', dash=None))
-
-
-	#print Story
+	for q in form_data["fields"]:
+		if q["ref"] in shown_responses.keys():
+			Story.append(Paragraph(u"<font color='#95C11E' name=Frutiger-65-Bold><b>{}</b></font>".format(q["title"].replace("*", "")),
+				styles["onepager_inner_heading"]))
+			if shown_responses[q["ref"]]["type"] == "choices":
+				for choice in shown_responses[q["ref"]]["choices"]["labels"]:
+					Story.append(Paragraph(u"{}".format(choice), 
+						styles["onepager_text"]))
+			elif shown_responses[q["ref"]]["type"] == "choice":
+				Story.append(Paragraph(u"{}".format(shown_responses[q["ref"]]["choice"][shown_responses[q["ref"]]["choice"].keys()[0]]), 
+					styles["onepager_text"]))
+			elif shown_responses[q["ref"]]["type"] == "url":
+				Story.append(Paragraph(u"{}".format(shown_responses[q["ref"]]["url"]), 
+					styles["onepager_text"]))
+			elif shown_responses[q["ref"]]["type"] == "email":
+				Story.append(Paragraph(u"{}".format(shown_responses[q["ref"]]["email"]), 
+					styles["onepager_text"]))
+			else:
+				try:
+					Story.append(Paragraph(u"{}".format(shown_responses[q["ref"]]["text"]), 
+						styles["onepager_text"]))
+				except KeyError as e:
+					print shown_responses[q["ref"]]
 	doc.build(Story)
 
+def get_form(form_id, api_key, raise_error=True):
+	base_url = "https://api.typeform.com/forms/"
+	form_url = "{}{}".format(base_url, form_id)
+	api_headers = {'Authorization': "Bearer {}".format(api_key)}
+	resp = requests.get(form_url, headers=api_headers)
+		
+	if resp.status_code != 200:
+		if raise_error:
+			resp.raise_for_status()
+		else:
+			return {}
+	
+	return resp.json()
+def get_responses(form_id, api_key, raise_error=True):
+	base_url = "https://api.typeform.com/forms/"
+	reponses_url = "{}{}/responses".format(base_url, form_id)
+	api_headers = {'Authorization': "Bearer {}".format(api_key)}
+	resp = requests.get(reponses_url, headers=api_headers)
+		
+	if resp.status_code != 200:
+		if raise_error:
+			resp.raise_for_status()
+		else:
+			return {}
+	
+	return resp.json()
+	
 if __name__ == "__main__":
 
-	responses_file = 'user_survey_data/responses.xlsx'
+	response_data = get_responses("mu9Pmx", INVENTORY_API_KEY)
+	responses = {}
+	for item in response_data["items"]:
+		if "answers" in item.keys():
+			if datetime.strptime(item["submitted_at"], "%Y-%m-%dT%H:%M:%SZ") > datetime.strptime("2019-05-17T00:00:01Z", "%Y-%m-%dT%H:%M:%SZ"):
+				responses[item["response_id"]] = item["answers"]
 
-	# Load spreadsheets
-	responses_xl = pd.ExcelFile(responses_file)
+	form_data = get_form("mu9Pmx", INVENTORY_API_KEY)
 
-	responses_df = responses_xl.parse(responses_xl.sheet_names[0])
+	for user_id in responses.keys():
+		generate_pdf(user_id, responses[user_id], form_data)
 
-	responses_data = dict()
-
-	for i, row in responses_df.iterrows():
-		responses_data[i] = dict()
-
-		responses_data[i]["01_id"] = unicode(row[0] if isinstance(row[0], basestring) else "")
-		responses_data[i]["02_name"] = unicode(row[1] if isinstance(row[1], basestring) else "")
-		responses_data[i]["03_position"] = unicode(row[2] if isinstance(row[2], basestring) else "")
-		responses_data[i]["04_email"] = unicode(row[3] if isinstance(row[3], basestring) else "")
-		responses_data[i]["05_affiliation"] = u";".join([x for x in list(row[4:30]) if isinstance(x, basestring)])
-		responses_data[i]["06_prev_platforms"] = u";".join([x for x in list(row[30:38]) if isinstance(x, basestring)])
-		responses_data[i]["07_propose"] = unicode(row[38] if isinstance(row[38], basestring) else "")
-		responses_data[i]["08_representing"] = unicode(row[39] if isinstance(row[39], basestring) else "")
-		responses_data[i]["09_description"] = unicode(row[40] if isinstance(row[40], basestring) else "")
-		responses_data[i]["10_platform_fit"] = u";".join([x for x in list(row[41:50]) if isinstance(x, basestring)])
-		responses_data[i]["11_num_users"] = unicode(row[50] if isinstance(row[50], basestring) else "")
-		responses_data[i]["12_additional_comments"] = unicode(row[51] if isinstance(row[51], basestring) else "")
-		responses_data[i]["13_start_date"] = unicode(row[52] if isinstance(row[52], basestring) else "")
-		responses_data[i]["14_submit_date"] = unicode(row[53] if isinstance(row[53], basestring) else "")
-		responses_data[i]["15_network_id"] = unicode(row[54] if isinstance(row[54], basestring) else "")
-
-	workbook = xlsxwriter.Workbook('user_survey_data/responses_parsed.xlsx')
-	worksheet_data = workbook.add_worksheet("Data")
-	bold = workbook.add_format({'bold': True})
-
-	for row, user in enumerate(sorted(responses_data.keys()), 1):
-		for col, item in enumerate(sorted(responses_data[user].keys()), 0):
-			if row == 1:
-				worksheet_data.write(0, col, item[3:], bold)
-			worksheet_data.write(row, col, responses_data[user][item])
-
-	workbook.close()
-
-	for user_responses in responses_data.keys():
-		generatePdf(responses_data[user_responses])
 
