5b428d860c84718804000b1d3be1551e60f327dc
Adrian <adr.lar@me.com>
2019-05-27 14:40:10 +0100

    vertical summary

--- inventory_survey.py
+++ inventory_survey.py
@@ -132,6 +132,94 @@ def generate_summary_pdf(summary_data, form_data, heading_colour="#0093BD"):
 	
 	doc.build(Story)
 
+def generate_summary_pdf_vertical(summary_data, form_data, heading_colour="#0093BD"):
+	
+	title = "Summary of {}".format(form_data["title"])
+	doc = BaseDocTemplate(u"user_survey_plots/summary_of_{}_vertical.pdf".format(title.lower().replace(" ", "_")),
+		pagesize=A4,
+		rightMargin=18*mm, leftMargin=18*mm,
+		topMargin=16*mm, bottomMargin=16*mm, 
+		showBoundary=0
+	)
+
+	pdfmetrics.registerFont(TTFont('MinionPro', 'MinionPro-Regular.ttf'))
+	pdfmetrics.registerFont(TTFont('Frutiger-65-Bold', 'Frutiger-LT-Std-65-Bold.ttf'))
+	pdfmetrics.registerFont(TTFont('Frutiger-45-Light', 'Frutiger-LT-Std-45-Light.ttf'))
+
+	styles = getSampleStyleSheet()
+	styles.add(ParagraphStyle(name="onepager_inner_heading", parent=styles["Heading1"], fontName="Frutiger-65-Bold", fontSize=10, color="#FF00AA", leading=16, spaceAfter=0, spaceBefore=8))
+	styles.add(ParagraphStyle(name="onepager_title", parent=styles["Heading1"], fontName="Frutiger-65-Bold", fontSize=16, bold=0, color="#000000", leading=16, spaceBefore=0))
+	styles.add(ParagraphStyle(name="onepager_text", parent=styles["Normal"], fontName="MinionPro", fontSize=10, bold=0, color="#000000", leading=14))
+
+	frame1 = Frame(doc.leftMargin, doc.bottomMargin, doc.width/2-3.5*mm, doc.height-21*mm, id='col1', leftPadding=0*mm, topPadding=0*mm, rightPadding=0*mm, bottomPadding=0*mm)
+	frame2 = Frame(doc.leftMargin+doc.width/2+3.5*mm, doc.bottomMargin, doc.width/2-3.5*mm, doc.height-21*mm, id='col2', leftPadding=0*mm, topPadding=0*mm, rightPadding=0*mm, bottomPadding=0*mm)
+	
+	header_content = Paragraph(
+		u'<b>{}</b>'.format(title), 
+		styles["onepager_title"])
+
+	header_item = PageTemplate(id='inventory_survey', frames=[frame1,frame2], onPage=partial(header, content=header_content))
+	
+	doc.addPageTemplates([header_item])
+
+	Story = []
+
+	for q in form_data["fields"]:
+		if q["ref"] in summary_data.keys():
+			Story.append(Paragraph(u"<font color='{}' name=Frutiger-65-Bold><b>{}</b></font>".format(heading_colour, q["title"].replace("*", "")),
+				styles["onepager_inner_heading"]))
+			
+			labels = list()
+			values = list()
+			for choice in q["properties"]["choices"]:
+				labels.append(textwrap.fill(choice["label"], 50).replace("\n", "<br>"))
+				values.append(0)
+			for item in summary_data[q["ref"]]:
+				try:
+					values[labels.index(textwrap.fill(item, 50).replace("\n", "<br>"))] += 1
+				except ValueError as e:
+					print e
+			data = [go.Bar(
+				y = values,
+				x = labels,
+			)]
+			layout = go.Layout(
+				margin = dict(
+					r = 200,
+					t = 20,
+					b = 200,
+					l = 20
+				),
+				yaxis = go.layout.YAxis(
+					tick0 = 0,
+					dtick = 1
+				),
+				xaxis=dict(
+					tickangle = 45
+				)
+			)
+			fig = go.Figure(data=data, layout=layout)
+			plotly.io.write_image(fig, u'user_survey_plots/{}.svg'.format(q["ref"]))
+
+			user_dist_filename = u'user_survey_plots/{}.svg'.format(q["ref"])
+
+			if os.path.isfile(user_dist_filename):
+				user_dist_image  = svg2rlg(user_dist_filename)
+
+				#Scaling the svg files
+				tmp_width = 83
+				tmp_height = 60
+
+				scaling_factor = tmp_width*mm/user_dist_image.width
+
+				user_dist_image.width = tmp_width*mm
+				user_dist_image.height = tmp_height*mm
+				user_dist_image.scale(scaling_factor, scaling_factor)
+				Story.append(Spacer(1, 3*mm))
+				Story.append(user_dist_image)
+			Story.append(CondPageBreak(60*mm))
+	
+	doc.build(Story)
 def generate_pdf(user_id, response, form_data, heading_colour="#0093BD"):
 	# print form_data
 	title = form_data["title"]
@@ -266,38 +354,40 @@ def get_responses(form_id, api_key, raise_error=True):
 	
 if __name__ == "__main__":
 
-	# mu9Pmx 95C11E
-	# TKMJZS 0093BD
+	form_ids = {"mu9Pmx": "#95C11E", "TKMJZS": "#0093BD"}
 
-	form_data = get_form("mu9Pmx", INVENTORY_API_KEY)
-	response_data = get_responses("mu9Pmx", INVENTORY_API_KEY)
-	
-	responses = dict()
-	for item in response_data["items"]:
-		if "answers" in item.keys():
-			if datetime.strptime(item["submitted_at"], "%Y-%m-%dT%H:%M:%SZ") > datetime.strptime("2019-05-17T00:00:01Z", "%Y-%m-%dT%H:%M:%SZ"):
-				responses[item["response_id"]] = item["answers"]
+	for form_id in form_ids.keys():
 
-	summary_responses = dict()
-	for item in responses:
-		for question in responses[item]:
-			if question["field"]["type"] == "multiple_choice":
-				if question["field"]["ref"] not in summary_responses.keys():
-					summary_responses[question["field"]["ref"]] = list()
-				if question["type"] == "choices":
-					for choice in question["choices"]["labels"]:
-						summary_responses[question["field"]["ref"]].append(choice)
-				elif question["type"] == "choice":
-					if "label" in question["choice"].keys():
-						summary_responses[question["field"]["ref"]].append(question["choice"]["label"])
+		form_data = get_form(form_id, INVENTORY_API_KEY)
+		response_data = get_responses(form_id, INVENTORY_API_KEY)
+		
+		responses = dict()
+		for item in response_data["items"]:
+			if "answers" in item.keys():
+				if datetime.strptime(item["submitted_at"], "%Y-%m-%dT%H:%M:%SZ") > datetime.strptime("2019-05-17T00:00:01Z", "%Y-%m-%dT%H:%M:%SZ"):
+					responses[item["response_id"]] = item["answers"]
+
+		summary_responses = dict()
+		for item in responses:
+			for question in responses[item]:
+				if question["field"]["type"] == "multiple_choice":
+					if question["field"]["ref"] not in summary_responses.keys():
+						summary_responses[question["field"]["ref"]] = list()
+					if question["type"] == "choices":
+						for choice in question["choices"]["labels"]:
+							summary_responses[question["field"]["ref"]].append(choice)
+					elif question["type"] == "choice":
+						if "label" in question["choice"].keys():
+							summary_responses[question["field"]["ref"]].append(question["choice"]["label"])
+						else:
+							summary_responses[question["field"]["ref"]].append("other")
 					else:
-						summary_responses[question["field"]["ref"]].append("other")
-				else:
-					exit("ERROR"+str(question))
+						exit("ERROR"+str(question))
 
-	generate_summary_pdf(summary_responses, form_data, heading_colour="#95C11E")
+		generate_summary_pdf(summary_responses, form_data, heading_colour=form_ids[form_id])
+		generate_summary_pdf_vertical(summary_responses, form_data, heading_colour=form_ids[form_id])
 
-	for user_id in responses.keys():
-		generate_pdf(user_id, responses[user_id], form_data, heading_colour="#95C11E")
+		for user_id in responses.keys():
+			generate_pdf(user_id, responses[user_id], form_data, heading_colour=form_ids[form_id])
 
 
